be9c3184df30dc80b29a5658eac206c341ae742f
Mustafa Mohamed <mo.mustafa3@gmail.com>
2020-07-21 21:42:29 +0100

    updated

--- server/service/email/quote/index.php
+++ server/service/email/quote/index.php
@@ -106,12 +106,11 @@
             ";
 
             // To send HTML mail, the Content-type header must be set
-            $headers[] = "MIME-Version: 1.0";
-            $headers[] = "Content-type: text/html; charset=iso-8859-1";
-            $headers[] = "To: <{$email}>";
-            $headers[] = "From: {$EMAIL_TITLE} <{$NO_REPLY_EMAIL}>";
+            $headers = "MIME-Version: 1.0" . "\r\n"; 
+            $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n"; 
+            $headers .= "From: $EMAIL_TITLE <$NO_REPLY_EMAIL>" . "\r\n"; 
 
-            mail('', $subject, $message, implode("\r\n", $headers));
+            mail($to, $subject, $message, $headers);
         }
 
         function quoteSubmitAdmin($quoteDetails){
@@ -224,12 +223,11 @@
             ";
 
             // To send HTML mail, the Content-type header must be set
-            $headers[] = "MIME-Version: 1.0";
-            $headers[] = "Content-type: text/html; charset=iso-8859-1";
-            $headers[] = "To: {$EMAIL_TITLE} support <{$to}>";
-            $headers[] = "From: {$EMAIL_TITLE} <{$SUPPORT_EMAIL}>";
+            $headers = "MIME-Version: 1.0" . "\r\n"; 
+            $headers .= "Content-type: text/html; charset=iso-8859-1" . "\r\n"; 
+            $headers .= "From: {$EMAIL_TITLE} <{$SUPPORT_EMAIL}>" . "\r\n"; 
 
-            mail($to, $subject, $message, implode("\r\n", $headers));
+            mail($to, $subject, $message, $headers);
         }
     }
 ?>
